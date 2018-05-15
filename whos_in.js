1a730a1cf79ecb376a9cd5b593fd3f361a7a2f0e
Andrew Peterson <andrewp2@umbc.edu>
2018-05-14 23:08:53 -0400

    added bulk of code

--- whos_in.js
+++ whos_in.js
@@ -1,6 +1,7 @@
 window.onload = pageLoad;
 
 function pageLoad() {
+    // setting the machines //
     cardio1 = $("cr_1");
     cardio2 = $("cr_2");
     cardio3 = $("cr_3");
@@ -10,6 +11,12 @@ function pageLoad() {
     cardio7 = $("cr_7");
     cardio8 = $("cr_8");
 
+    weight1 = $("wrr_1");
+    weight2 = $("wrr_2");
+    weight3 = $("wrr_3");
+    weight4 = $("wrr_4");
+
+    // setting the hover events for each machine //
     cardio1.onmouseover = mouseOverc1; //will check the output of the other php file to see if available and turn it red or green
     cardio1.onmouseout = mouseOutc1; //will just grey out the box
 
@@ -33,84 +40,291 @@ function pageLoad() {
 
     cardio8.onmouseover = mouseOverc8;
     cardio8.onmouseout = mouseOutc8;
+
+    weight1.onmouseover = mouseOverw1;
+    weight1.onmouseout = mouseOutw1;
+
+    weight2.onmouseover = mouseOverw2;
+    weight2.onmouseout = mouseOutw2;
+
+    weight3.onmouseover = mouseOverw3;
+    weight3.onmouseout = mouseOutw3;
+
+    weight4.onmouseover = mouseOverw4;
+    weight4.onmouseout = mouseOutw4;
 }
 
-function checkPassword() {
-    var password = $("password").value;
-    new Ajax.Request("strongornot.php", {
-        method: "get",
-        parameters: {
-            pass: password
-        },
-        onSuccess: displayStrength
-    });
-}
-
-function displayStrength(ajax) {
+function displayWeightInfo(ajax) {
     var r = ajax.responseText;
-    $('comment').innerHTML = r;
+    $('info').innerHTML = r;
+
+    var str = "Hello world, welcome to the universe.";
+    var n = r.includes("nobody"); //generally means there is nobody on the machine currently
+    var c = r.includes("not open yet"); //if you are not in the correct hours, this will be in the string
+
+    if (n && !c) {
+        $('info').style.background = 'green';
+    } else {
+        $('info').style.background = '#ff3333';
+    }
 }
 
 function mouseOverc1() {
-    cardio1.style.background = "green"
+    cardio1.style.background = "gold"
+    var c_machine = 1;
+    var w_machine = 0;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
 }
 
 function mouseOutc1() {
     cardio1.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
 }
 //
 function mouseOverc2() {
-    cardio2.style.background = "green"
+    cardio2.style.background = "gold"
+    var c_machine = 2;
+    var w_machine = 0;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
 }
 
 function mouseOutc2() {
     cardio2.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
 }
 //
 function mouseOverc3() {
-    cardio3.style.background = "green"
+    cardio3.style.background = "gold"
+    var c_machine = 3;
+    var w_machine = 0;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
 }
 
 function mouseOutc3() {
     cardio3.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
 }
 //
 function mouseOverc4() {
-    cardio4.style.background = "green"
+    cardio4.style.background = "gold"
+    var c_machine = 4;
+    var w_machine = 0;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
 }
 
 function mouseOutc4() {
     cardio4.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
 }
 //
 function mouseOverc5() {
-    cardio5.style.background = "green"
+    cardio5.style.background = "gold"
+    var c_machine = 5;
+    var w_machine = 0;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
 }
 
 function mouseOutc5() {
     cardio5.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
 }
 //
 function mouseOverc6() {
-    cardio6.style.background = "green"
+    cardio6.style.background = "gold"
+    var c_machine = 6;
+    var w_machine = 0;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
 }
 
 function mouseOutc6() {
     cardio6.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
 }
 //
 function mouseOverc7() {
-    cardio7.style.background = "green"
+    cardio7.style.background = "gold"
+    var c_machine = 7;
+    var w_machine = 0;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
 }
 
 function mouseOutc7() {
     cardio7.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
 }
 //
 function mouseOverc8() {
-    cardio8.style.background = "green"
+    cardio8.style.background = "gold"
+    var c_machine = 8;
+    var w_machine = 0;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
 }
 
 function mouseOutc8() {
     cardio8.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
+}
+
+/* 
+
+BELOW ARE THE FUNCTIONS FOR THE WEIGHT ROOM MACHINES 
+
+*/
+
+function mouseOverw1() {
+    weight1.style.background = "gold"
+    var c_machine = 0;
+    var w_machine = 9;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
+}
+
+
+function mouseOutw1() {
+    weight1.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
+}
+//
+function mouseOverw2() {
+    weight2.style.background = "gold"
+    var c_machine = 0;
+    var w_machine = 10;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
+}
+
+function mouseOutw2() {
+    weight2.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
+}
+//
+function mouseOverw3() {
+    weight3.style.background = "gold"
+    var c_machine = 0;
+    var w_machine = 11;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
+}
+
+
+function mouseOutw3() {
+    weight3.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
+}
+//
+function mouseOverw4() {
+    weight4.style.background = "gold"
+    var c_machine = 0;
+    var w_machine = 12;
+
+    new Ajax.Request("whos_in_test.php", {
+        method: "get",
+        parameters: {
+            cardio_id: c_machine,
+            weights_id: w_machine
+        },
+        onSuccess: displayWeightInfo
+    });
+}
+
+function mouseOutw4() {
+    weight4.style.background = "grey"
+    $('info').style.background = "grey";
+    $('info').innerHTML = '';
 }
