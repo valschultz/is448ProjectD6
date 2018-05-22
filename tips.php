36031df226cfec5e774431b6c83841f11619fdf3
mbrooks3 <mbrooks3@umbc.edu>
2018-05-22 19:55:59 -0400

    Update tips.php

--- tips.php
+++ tips.php
@@ -1,10 +1,13 @@
 <!--Author: Mary Brooks -->
 <?php
+
+//connect to the databse
 $db = mysqli_connect("studentdb-maria.gl.umbc.edu","mbrooks3","mbrooks3","mbrooks3");
 	
 	if (mysqli_connect_errno())	{
 		exit("Error - could not connect to MySQL");
 	}
+//get random tip from the databse table "Tips"
 $number = floor(rand(1,10));
 $tip_query = "SELECT tip_content FROM Tips WHERE tip_id = '".$number."'";
 $tip_result = mysqli_query($db,$tip_query);
